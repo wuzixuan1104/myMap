c52a2a337a7ded41d2eb1450375da6a103b74e29
吳姿萱 <cherry51120@gmail.com>
2018-06-27 13:16:47 +0800

    update again

--- js/public.js
+++ js/public.js
@@ -6,87 +6,12 @@
  */
 
 $(function () {
-  function initBox(r){
-    var $tmp = $(r);
-    $tmp.find('.xxx').click(function(){
-      alert();
-    })
-    return $tmp;
-  }
-
-  $.get('sidemenu.html', function(r) {
-    $('#sidemenu').append(initBox(r));
+  $.get('_sidemenu.html', function(r) {
+    $('#sidemenu').append($(r));
   });
-
-  // var sidemenu = [
-  //   {title: 'Tutorial', items: [
-  //     {text: 'A', href: ''},
-  //     {text: 'B', href: ''},
-  //     {text: 'C', href: ''},
-  //     {text: 'D', href: ''},
-  //   ]},
-  //   {title: 'Tutorial', items: [
-  //     {text: 'E', href: ''},
-  //     {text: 'F', href: ''},
-  //     {text: 'G', href: ''},
-  //     {text: 'H', href: ''},
-  //   ]},
-  //   {title: 'Tutorial', items: [
-  //     {text: 'I', href: ''},
-  //     {text: 'J', href: ''},
-  //     {text: 'K', href: ''},
-  //     {text: 'L', href: ''},
-  //   ]},
-  // ];
-  //
-  //
-  //   $('#sidemenu').append($('<div/>').addClass ('title').text('Google Map Application')).append($('<div/>').addClass ('boxes').append(
-  //     sidemenu.map(function(box) {
-  //
-  //       var items = box.items.map(function (item) {
-  //         return $('<a/>').text(item.text);
-  //       });
-  //
-  //       items = $(items).map ($.fn.toArray);
-  //
-  //       return $('<div/>').addClass('box').append(
-  //         $('<b>').text(box.title).add(items)
-  //       );
-  //     })
-  //   ));
-  //
-  //
-  //
-  // $('#sidemenu').append($('<div/>').addClass ('title').text('Google Map Application')).append($('<div/>').addClass ('boxes').append(
-  //   sidemenu.map(function(box) {
-  //     console.log(box);
-  //
-  //     var $box = $('<div/>').addClass('box').append(
-  //       $('<b>').text(box.title)
-  //     );
-  //
-  //     var items = box.items.map(function (item) {
-  //       return $('<a/>').text(item.text).appendTo($box);
-  //     });
-  //
-  //     return $box;
-  //   })
-  // ));
-
-  // $('#sidemenu').append (
-  //   $('<div/>').addClass ('title').text('Google Map Application')
-  // ).append(
-  //   $('<div/>').addClass ('boxes').append(
-  //     $('<div/>').addClass ('box').append(
-  //       $('<b>').text('Tutorial')
-  //     ).append(
-  //       $('<a>').text('~~~~~~~~')
-  //     ).append(
-  //       $('<a>').text('~~~~~~~~')
-  //     ).append(
-  //       $('<a>').text('~~~~~~~~')
-  //     )
-  //   )
-  // );
+  var today = new Date();
+  if( $('.folder').length > 0 ) {
+    $('.folder').append( '<span class="date">' + today.toISOString().substring(0, 10) + '</span>');
+  }
 
 });
