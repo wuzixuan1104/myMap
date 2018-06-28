519be5d17d1ed61e2d39f05d5670b52969be6f88
吳姿萱 <cherry51120@gmail.com>
2018-06-28 16:09:52 +0800

    modify

--- js/public.js
+++ js/public.js
@@ -6,20 +6,17 @@
  */
 
 $(function () {
-  var $a = null;
+  $a = null;
+  $current = $(location).attr('href').split('/').pop();
 
-  var current = $(location).attr('href').split('/').pop();
-
-  $.get('sidemenu.html', function(r) {
-    var $tmp = $(r);
+  if( localStorage && comparePasstime(localStorage.getItem("menu_datetime"), 10) && ( localStorage.getItem("menu") ) ) {
+    var $tmp = $( localStorage.getItem("menu") );
+    $tmp.find("a[href$='" + $current + "']").addClass('active');
     $a = $tmp.find('.box a');
     $('#sidemenu').append($tmp);
-    $a.each(function () {
-      if ($(this).attr('href') == (current == '' ? 'index.html' : current) )
-        $(this).addClass('active');
-    });
-
-  });
+  } else {
+    sidemenuHandler();
+  }
 
   if( $('.date').length <= 0 ) {
     var today = new Date();
@@ -35,10 +32,8 @@ $(function () {
   $('.icon-chevron-thin-up').click(function() {
     if ($a === null)
       return false;
-
     if ((index = $a.index( $a.filter('.active') )) === -1)
       return false;
-
     index = --index < 0 ? $a.length - 1 : index;
     window.location.assign($a.eq(index).attr('href'));
   });
@@ -46,14 +41,48 @@ $(function () {
   $('.icon-chevron-thin-down').click(function() {
     if ($a === null)
       return false;
-
     if ((index = $a.index( $a.filter('.active') )) === -1)
       return false;
-
     index = ++index > $a.length - 1 ? 0 : index;
     window.location.assign($a.eq(index).attr('href'));
   });
 
+  //  下午加入選到的話左邊選單active特效
+  $(document).keydown(function(e){
+    if (e.keyCode == 38) {
+      if ($a === null)
+        return false;
+      if ((index = $a.index( $a.filter('.active') )) === -1)
+        return false;
+      index = --index < 0 ? $a.length - 1 : index;
+      window.location.assign($a.eq(index).attr('href'));
 
-
+    } else if (e.keyCode == 40) {
+      if ($a === null)
+        return false;
+      if ((index = $a.index( $a.filter('.active') )) === -1)
+        return false;
+      index = ++index > $a.length - 1 ? 0 : index;
+      window.location.assign($a.eq(index).attr('href'));
+    }
+  });
 });
+
+function sidemenuHandler() {
+  $.get('sidemenu.html', function(r) {
+    if(localStorage) {
+      localStorage.setItem('menu', r );
+      localStorage.setItem('menu_datetime', new Date() );
+    }
+    var $tmp = $(r);
+    $tmp.find("a[href$='" + $current + "']").addClass('active');
+    $a = $tmp.find('.box a');
+    $('#sidemenu').append($tmp);
+  });
+}
+
+function comparePasstime(date, minutes) {
+  if( new Date() <= new Date( new Date(date).getTime() + minutes*60000 ) )
+    return true;
+  return false;
+}
